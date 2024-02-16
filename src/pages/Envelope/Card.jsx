5b49b623dbe66b12ca5841313dffe626cd528fe5
Benedict Aurelius <aurelius1908@gmail.com>
2024-02-16 15:53:16 +0700

    add some effects

--- src/pages/Envelope/Card.jsx
+++ src/pages/Envelope/Card.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Envelope from '/src/assets/envelope.png';
 import LoveBackground from '/src/assets/love.svg';
 
@@ -7,18 +8,22 @@ function Card() {
       <section id="CardPage">
         <div className=" ">
           <img src={LoveBackground} alt="lovenya doang" className="absolute bottom--40 left-1/2 transform -translate-x-1/2 scale-150" />
-          <div className="text-wrapper absolute top-5 left-1/2 transform -translate-x-1/2 scale-125 mt-16">
-            <a href="/">
+            <div className="text-wrapper absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-125">
+                <a href="/">
                 {/* nanti di ganti route nya yaa */}
-              <img src={Envelope} alt="envelope" />
-            </a>
-            <div className="text-6xl font-fredokaone text-color mb-3 bottom-0 text-center">
-              <p><b>You have a message !</b></p>
+                <motion.img 
+                    src={Envelope} 
+                    alt="envelope" 
+                    whileHover={{ rotate: [0, -5, 5, -5, 0], transition: { duration: 0.3 } }}
+                />
+                </a>
+                <div className="text-6xl font-fredokaone text-color mb-3 bottom-0 text-center">
+                    <p><b>You have a message !</b></p>
+                </div>
+                <div className="text-2xl text-color text-center font-poppins">
+                    <p>( click envelope to open )</p>
+                </div>
             </div>
-            <div className="text-2xl text-color text-center font-poppins">
-              <p>( click envelope to open )</p>
-            </div>
-          </div>
         </div>
       </section>
     </div>
