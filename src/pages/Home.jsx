25d9d56c150650bb6316ff14cc66a89b2e5ba957
rastuak <rastuakfaisal@gmail.com>
2024-02-16 20:25:12 +0700

    fixed lovebg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import $ from 'jquery'; 
+import $ from 'jquery';
 import Heart from "./../assets/Heart.png"
 import Surat from "./../assets/surat.png"
 import Buletbawah from "./../assets/buletbulet.png"
@@ -24,11 +24,11 @@ const Home = () => {
         }
     }
 
-    $( "#header-plugin" ).load( "https://vivinantony.github.io/header-plugin/", function() {
-	$("a.back-to-link").attr("href", "http://blog.thelittletechie.com/2015/03/love-heart-animation-using-css3.html#tlt")  
+    $("#header-plugin").load("https://vivinantony.github.io/header-plugin/", function () {
+        $("a.back-to-link").attr("href", "http://blog.thelittletechie.com/2015/03/love-heart-animation-using-css3.html#tlt")
     });
 
-    var love = setInterval(function() {
+    var love = setInterval(function () {
         var r_num = Math.floor(Math.random() * 40) + 1;
         var r_size = Math.floor(Math.random() * 65) + 10;
         var r_left = Math.floor(Math.random() * 100) + 1;
@@ -39,7 +39,7 @@ const Home = () => {
 
         $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
 
-        $('.heart').each(function() {
+        $('.heart').each(function () {
             var top = $(this).css("top").replace(/[^-\d\.]/g, '');
             var width = $(this).css("width").replace(/[^-\d\.]/g, '');
             if (top <= -100 || width >= 150) {
@@ -53,7 +53,7 @@ const Home = () => {
         <>
             <div className='header-plugin'>
                 <div className='bg_heart overflow-hidden bg-gradient-to-b from-pink1 to-pink2 min-h-screen justify-center items-center flex relative'>
-                    <img src={Buletbawah} alt="bulet bulet bawah" className="opacity-70 absolute bottom-0 left-0 w-full h-auto lg:h-1/3 z-0" />
+                <img src={Buletbawah} alt="bulet bulet bawah" className="fixed bottom-0 left-0 w-full h-auto lg:h-1/3 z-10" />
                     <img src={Surat} alt="kotaknya doang" className="z-10 fixed flex-none md:top-20 lg:top-4 top-32" />
                     <h1 className='fixed flex-none top-48 md:top-60 lg:top-36 z-20 lg:text-[105px] md:text-[75px] text-[50px] text-brown1 font-titanone font-normal justify-center items-center flex flex-row'>
                         VALEN
