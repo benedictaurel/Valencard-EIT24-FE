4250275bb2c1ecae7d0ec1e016214dd79c7265e8
eln_billie <me.billie@yahoo.com>
2024-02-17 11:48:23 +0700

    Formulir now can rendered based on Submitted

--- src/pages/Formulir.jsx
+++ src/pages/Formulir.jsx
@@ -1,18 +1,26 @@
-
+import React, { useState } from 'react';
 import GenerateElem from './../components/FormElem';
 import SuccessElem from './../components/SuccesElem';
 const Formulir = () => {
-
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [cardKey, setCardKey] = useState('');
+    
+    const handleSuccess = (key) => {
+      setCardKey(key);
+      setIsSubmitted(true);
+    };
 
   return (
     <>
       <div className='flex justify-center items-center min-h-screen bg-gradient-to-t from-bgsuccess to-tobgsuccess lg:px-48 md:px-16 px-8 py-16 md:py-24 lg:py-32 overflow-hidden'>
       
-        {/* component buat generate */}
-        <GenerateElem/> 
-
-        {/* component buat success generate */}
-        {/* <SuccessElem/> */}
+      {isSubmitted ? (
+          // Show the success element if submitted
+          <SuccessElem cardKey={cardKey} />
+        ) : (
+          // Otherwise, show the form element
+          <GenerateElem onSuccess={handleSuccess} />
+        )}
       
       </div>
     </>
