5802eb0bac5ca44a4a871ecb1fa873f0dabfb5a0
rastuak <rastuakfaisal@gmail.com>
2024-02-18 18:21:00 +0700

    fixed mainpage button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Buletbawah from "./../assets/buletbulet.png"
 import Buttoncode from "./../assets/buttoncode.png"
 import { useRef, useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { PaperPlaneRight } from "@phosphor-icons/react";
+
 
 const Home = () => {
     const searchRef = useRef()
@@ -28,16 +30,16 @@ const Home = () => {
     }
 
     const handleCodeChange = (e) => {
-        const input = e.target.value  ;
+        const input = e.target.value;
         const inputFormatted = input.replace(/[^0-9a-zA-Z]/g, '');
-        setCodeFormat(inputFormatted); 
+        setCodeFormat(inputFormatted);
     }
-    
+
     useEffect(() => {
         startLoveAnimation();
-        return () => clearInterval(loveInterval); 
+        return () => clearInterval(loveInterval);
     }, []);
-    
+
     const startLoveAnimation = () => {
         const loveInterval = setInterval(function () {
             var r_num = Math.floor(Math.random() * 5) + 1;
@@ -47,7 +49,7 @@ const Home = () => {
             var r_time = Math.floor(Math.random() * 9) + 5;
 
             $("#header-plugin").load("https://vivinantony.github.io/header-plugin/", function () {
-            $("a.back-to-link").attr("href", "http://blog.thelittletechie.com/2015/03/love-heart-animation-using-css3.html#tlt")
+                $("a.back-to-link").attr("href", "http://blog.thelittletechie.com/2015/03/love-heart-animation-using-css3.html#tlt")
             });
 
             $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
@@ -69,7 +71,7 @@ const Home = () => {
         <>
             <div className='header-plugin'>
                 <div className='bg_heart overflow-hidden bg-gradient-to-bl from-[#FF9B98] to-[#FFE3E8] min-h-screen justify-center items-center flex relative'>
-                <img src={Buletbawah} alt="bulet bulet bawah" className="fixed bottom-0 left-0 w-full h-auto lg:h-1/3 z-10" />
+                    <img src={Buletbawah} alt="bulet bulet bawah" className="fixed bottom-0 left-0 w-full h-auto lg:h-1/3 z-10" />
                     <img src={Surat} alt="kotaknya doang" className="z-10 fixed flex-none md:top-20 lg:top-4 top-32" />
                     <h1 className='fixed flex-none top-48 md:top-60 lg:top-36 z-20 lg:text-[105px] md:text-[75px] text-[50px] text-brown1 font-titanone font-normal justify-center items-center flex flex-row'>
                         VALEN
@@ -84,19 +86,19 @@ const Home = () => {
                                 onKeyDown={handleSearchCode}>
                                 <input ref={searchRef}
                                     placeholder="- - - - - - - - - -"
-                                    value = {codeFormat}
-                                    onChange = {(e) => handleCodeChange(e)}
-                                    maxLength={10} 
+                                    value={codeFormat}
+                                    onChange={(e) => handleCodeChange(e)}
+                                    maxLength={10}
                                     className='p-2 text-center sm:text-lg lg:text-3xl font-normal font-kleeone w-[70%] lg:w-[650px] lg:h-[90px] rounded-full mb-2 shadow-xl shadow-shadowbtn bg-krem1 focus:outline-0'
                                 />
                             </form>
-                            <button className="absolute text-gray-700 right-0 md:right-[68px] lg:right-0 top-[0.8px] transition-all duration-300 hover:scale-[1.025]">
-                                <img src={Buttoncode} onClick={handleSearchCode} className="size-[45%] md:size-[49%] lg:size-auto" />
-                            </button>
+                            <div onClick={handleSearchCode} className='absolute right-12 md:right-[113px] lg:right-1 text-center justify-center items-center flex top-0 transition-all duration-300 hover:scale-[1.025] rounded-full bg-[#F0C0C0] text-white hover:text-[#F0C0C0] hover:bg-hvrkrem1 h-[40px] w-[40px] md:h-[45px] md:w-[45px] lg:h-[90px] lg:w-[90px] shadow-xl'>
+                                <PaperPlaneRight size={28} />
+                            </div>
                         </div>
                         <h1 className='text-xl font-kleeone mb-1'>or</h1>
                         <Link to="/generate">
-                            <button className='p-3 text-center lg:text-xl font-normal font-kleeone w-[71%] lg:w-[660px] lg:h-[90px] rounded-full mb-2 shadow-xl shadow-shadowbtn bg-krem1 hover:bg-hvrkrem1 transition-all duration-300 hover:scale-[1.015]'>
+                            <button className='p-3 text-center lg:text-xl font-normal font-kleeone w-[71%] lg:w-[660px] lg:h-[90px] rounded-full mb-2 shadow-xl shadow-shadowbtn bg-krem1 hover:bg-hvrkrem1 transition-all duration-300 hover:scale-[1.005]'>
                                 make your own card now!
                             </button>
                         </Link>
